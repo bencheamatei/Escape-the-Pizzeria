f493b6b7638b9d010c46417ef640e8c42a1ff932
Matei <mateibenchea2@gmail.com>
2026-05-12 11:36:05 +0300

    added abstract scene class

---
+++ include/scene.h
@@ -0,0 +1,26 @@
+//
+// Created by matei on 5/12/2026.
+//
+
+#ifndef OOP_SCENE_H
+#define OOP_SCENE_H
+
+#include <SFML/Graphics.hpp>
+class game;
+
+class scene {
+protected:
+    game& _game;
+    virtual void on_update(float)=0;
+    virtual void on_render(sf::RenderWindow&)=0;
+    virtual void on_event(const sf::Event&)=0;
+public:
+    explicit scene(game&);
+    virtual ~scene();
+    void update(float);
+    void render(sf::RenderWindow&);
+    void event_handler(const sf::Event&);
+};
+
+
+#endif //OOP_SCENE_H
